9e597337e8de2792fc04c3fa79d9b88f07bba20b
Daniel Badan <badanesc@gmail.com>
2025-05-03 11:48:07 +0200

    Make filters card sticky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div>
         <Header />
         <Providers>{children}</Providers>
-        </div>
       </body>
     </html>
   );
