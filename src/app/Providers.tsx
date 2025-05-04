1081beba19966dd5a721a8d8659b8904a7515f2a
Daniel Badan <badanesc@gmail.com>
2025-05-04 10:24:09 +0200

    Move getQueryClient function to separate file

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,33 +1,11 @@
 'use client'
 
 import {
-  QueryClient,
   QueryClientProvider,
-  isServer,
 } from '@tanstack/react-query'
 import { PropsWithChildren } from "react";
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
-
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  })
-}
-
-let browserQueryClient: QueryClient | undefined = undefined
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient()
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient()
-    return browserQueryClient
-  }
-}
+import { getQueryClient } from '@/app/getQueryClient'
 
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
   const queryClient = getQueryClient()
