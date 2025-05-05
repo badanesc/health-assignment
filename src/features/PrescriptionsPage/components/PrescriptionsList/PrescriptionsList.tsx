814c64634a614ef24fcfa6dd91d1b066197e4b8e
Daniel Badan <badanesc@gmail.com>
2025-05-05 11:20:25 +0200

    Remove fragment

--- src/features/PrescriptionsPage/components/PrescriptionsList/PrescriptionsList.tsx
+++ src/features/PrescriptionsPage/components/PrescriptionsList/PrescriptionsList.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Fragment } from "react";
 import { useFilteredPrescriptions } from "../../hooks";
 import { PrescriptionCard } from "../PrescriptionCard";
 import styles from "./PrescriptionsList.module.css";
@@ -16,9 +15,7 @@ const PrescriptionsList = () => {
   return (
     <ul className={styles.list}>
       {prescriptions.map((prescription) => (
-        <Fragment key={prescription.id}>
-          <PrescriptionCard prescription={prescription} />
-        </Fragment>
+        <PrescriptionCard key={prescription.id} prescription={prescription} />
       ))}
     </ul>
   );
